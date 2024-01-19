cfe76b542e89f9b44c9a79e349b1f93dcec7d113
Marcos Tavares <dev.marcostavares@gmail.com>
2024-01-19 18:52:28 -0300

    fix: fixing the sign up component not being visible on mobile devices

--- src/components/core/Container/Container.tsx
+++ src/components/core/Container/Container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export default function Container({ children }: Props) {
     return (
-        <div className="flex h-lvh w-full p-4 antialiased items-center justify-center">
+        <div className="flex h-lvh w-full p-4 max-[768px]:my-28 antialiased items-center justify-center">
             {children}
         </div>
     );
